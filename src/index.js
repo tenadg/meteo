9e2c879175f414c8dc4b0ce95bddc53bc9714b1f
tenadg <design.tena@gmail.com>
2024-05-07 00:37:39 -0400

    Added the search functionality using the API

--- src/index.js
+++ src/index.js
@@ -1,9 +1,27 @@
+function refreshWeather(response) {
+    let temperatureElement = document.querySelector(#"temperature");
+    let temperature = response.data.temperature.current;
+    temperatureElement.innerHTML = response.data.temperature.current;
+    let cityElement = document.querySelector("#city");
+    cityElement.innerHTML = response.data.city;
+    temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function searchCity(city) {
+  let apiKey = "3ff6cbf7904o3b69bt917218508fcdac";
+  let apiURL = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiURL).then(refreshWeather);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Lisbon:");
